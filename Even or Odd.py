8d7be00b3708bb38d8efdd31e144405c10769d61
marwenerzig <86536189+marwenerzig1@users.noreply.github.com>
2024-09-19 23:51:47 +0100

    Add files via upload
    save Even or Odd

---
+++ Even or Odd.py
@@ -0,0 +1,12 @@
+#Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers
+
+def odd_or_even(x):
+    if (x % 2 == 0):
+        print(x, "is an Even number!")
+    else:
+        print(x, "is an Odd number!")
+
+ 
+
+
+odd_or_even(5)
